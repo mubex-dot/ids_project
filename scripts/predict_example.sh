0c9ecac203421430eb825b4fbb3c9b61fe37f8fc
mubex-dot <63114194+mubex-dot@users.noreply.github.com>
2026-02-10 17:11:35 +0100

    fixed correct path for app/models files

--- scripts/predict_example.sh
+++ scripts/predict_example.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python src/models/infer.py --model models/best_dt.joblib --json '{
+python src/app/models/infer.py --model models/best_dt.joblib --json '{
   "protocol_type":"tcp", "service":"http", "flag":"SF",
   "src_bytes": 181, "dst_bytes": 5450, "count": 2, "srv_count": 2
 }'
